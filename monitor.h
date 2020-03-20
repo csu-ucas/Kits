57d6e4574ef920cbedbd6867f2634f5b282555bd
Xin Wang <hsimwong1202@gmail.com>
2020-03-20 14:51:20 +0800

    Update monitor.h: Specified functions to be implemented

--- monitor.h
+++ monitor.h
@@ -1,6 +1,9 @@
 #ifndef MONITOR_H
 #define MONITOR_H
 
+#include <string>
+#include "sensor.h"
+#include "communicator.h"
 enum State {
     ACTIVE,
     DEAD,
@@ -11,15 +14,35 @@ enum State {
 class Monitor {
 private:
     int monitor_id;
+    Communicator * cmt;
+    
     /* Private functions that are limited-used by
-     * class-internal process. */
+    * class-internal process. */
     State get_sensor_status();
+
     float get_cpu_workload();
+    
     State get_FPGA_status();
+
+    // Fetching directly from docker
+    State fetch_schedule_result();
+
+    // Fetching such info from c-agent
+    std::string fetch_schedule_info();
+
+    // Writting to etcd
+    bool send_pulse_etcd();
+    
+    // Sending a copy of info that are originally
+    // to etcd to c-agent
+    bool send_pulse_cagent();
+    
 
 public:
     Monitor();
+
     ~Monitor();
+
     bool run(); 
 };
 
